1830f947d8507e552bb70044a239e42442d48ecd
Joshua <wilson.joshua@outlook.com>
2021-09-01 11:26:43 -0600

    commit tree 8c46a8e5a28246f1a166d5d973aab610f39d89ff

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
 
+//  function superbowlWin(record, year, result) {
+//      return (year, result = "1969")
+//  }
+
+// function supernowWin (record)
+
+
+
+// function superbowlWin (record, ) {
+//     record.find(Element => Element === "W");
+//     return record.find(result => result === "W" )
+// }
+function superbowlWin(record) {
+    return record.result === "W"
+}
+
+
+console.log(record.find(superbowlWin).year);
